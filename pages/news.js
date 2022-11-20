3b23f92218612fd845e06c8434ae0ca543f99ebc
Jake Doan <jakedoan3@gmail.com>
2022-11-20 17:24:51 -0600

    started news component sass

--- pages/news.js
+++ pages/news.js
@@ -3,9 +3,9 @@ import React from 'react'
 const News = () => {
   return (
     <div>
-      <title>ell!psis | News</title>
       <h1>News</h1>
       <p>Lorem ipsum</p>
+      {/* Apply card style to each news item */}
     </div>
   )
 }

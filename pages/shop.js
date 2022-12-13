b393a934886c1f53d59c997a99fba2269da41886
Jake Doan <jakedoan3@gmail.com>
2022-12-12 23:44:17 -0600

    added stripe

--- pages/shop.js
+++ pages/shop.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import {checkout} from "./checkout"
 
 const Shop = () => {
   return (
     <div>
       <title>ell!psis | Shop</title>
       <h1>Shop</h1>
-      
-      <a href='https://www.google.com' className='storeItem'>
-        <Image src='/images/samplepackbundle.jpg' width={250} height={250}/>
-        <h3>Sample packs 1-3</h3>
-        <p>They're really good!</p>
-      </a>
-      
-      <a href='https://www.google.com' className='storeItem'>
-      <Image src='/images/archoncover.jpg' width={250} height={250}/>
-        <h3>Archon USB</h3>
-        <p>LIMITED QUANTITY: Contains "archon" EP, Sample Packs 1-3, full "archon" stems, and visuals by jersea</p>
-      </a>
-      
-    </div>
-  )
-}
 
-export default Shop
+      <div>
+        <Image src="/images/samplepackbundle.jpg" alt="sample pack bundle" width={250} height={250} />
+        <h3>Sample Pack Vol 1-3 Bundle</h3>
+        <p>
+          Hundreds of original drums, fx, glitches, loops, and more, all bundled
+          together from my 3 sample packs to date
+        </p>
+        <button onClick={(()=> {
+          checkout({
+            lineItems: [
+              {
+                price: "price_1MEQH7JEOsoohAF84SRZ4INu",
+                quantity: 1
+              }
+            ]
+          })
+        })}>Buy</button>
+      </div>
+
+      <div>
+        <Image src="/images/archoncover.jpg" alt="archon cover" width={250} height={250} />
+        <h3>Archon USB</h3>
+        <p>
+          LIMITED QUANTITY: Contains "archon" EP, Sample Packs 1-3, full
+          "archon" stems, and visuals by jersea
+        </p>
+        
+        <button onClick={(()=> {
+          checkout({
+            lineItems: [
+              {
+                price: "price_1MEQHuJEOsoohAF8rLKqC4J6",
+                quantity: 1
+              }
+            ]
+          })
+        })}>Buy</button>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;

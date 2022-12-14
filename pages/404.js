0559f5ecc061512e6cf869424e3a9623ffd3753b
Jake Doan <jakedoan3@gmail.com>
2022-12-14 14:18:59 -0600

    commented out

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react'
+// import React, {useEffect, useRef, useState} from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useRouter } from 'next/router'

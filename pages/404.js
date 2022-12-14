d3d78d50d74f7ae359d600e2a44ea4e2ffad2188
Jake Doan <jakedoan3@gmail.com>
2022-12-14 14:20:23 -0600

    commented out useEffect

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,16 @@
 // import React, {useEffect, useRef, useState} from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
 
 const NotFound = () => {
     const router = useRouter();
 
-    useEffect(() => {
-        setTimeout(() => {
-            router.push('/')
-        },3000)
-    },[])
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         router.push('/')
+    //     },3000)
+    // },[])
   return (
     <div className='not-found'>
         <title>ell!psis | Page not found </title>
